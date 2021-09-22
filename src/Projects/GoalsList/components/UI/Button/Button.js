e7aac4f80b2d21575cdabda349a91fd249523295
sahilbatra158 <73225734+sahilbatra158@users.noreply.github.com>
2021-09-22 12:48:34 +0530

    Used Router and added ExpenseTracker Project and GoalList Project yet to be configured for css modules (css module not running yet in it)

---
+++ src/Projects/GoalsList/components/UI/Button/Button.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import styles from './Button.module.css'
+
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #256aff;
+//   color: white;
+//   background: #256aff;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #0084ff;
+//     border-color: #0066ff;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
